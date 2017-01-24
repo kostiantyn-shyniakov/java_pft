3211a76717459c6fcece355ac819e75132acb6f1
kostiantyn-shyniakov <shinyakov@gmail.com>
2017-01-24 18:55:19 +0200

    Lesson 9

---
+++ rest-sample/src/test/java/pft/rest/RestTests.java
@@ -0,0 +1,35 @@
+package pft.rest;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+import com.google.gson.reflect.TypeToken;
+import org.apache.http.client.fluent.Executor;
+import org.apache.http.message.BasicNameValuePair;
+import org.testng.annotations.Test;
+import org.apache.http.client.fluent.Request;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Set;
+
+import static org.testng.Assert.assertEquals;
+
+/**
+ * Created by kshyniakov on 24.01.2017.
+ */
+public class RestTests extends TestBase {
+
+    @Test
+    public void testCreateIssue() throws IOException {
+        skipIfNotFixed(5);
+        Set<Issue> oldIssues = getIssues();
+        Issue newIssue = new Issue().withSubject("Test subject").withDescription("Test description");
+        int issueId = createIssue(newIssue);
+        Set<Issue> newIssues = getIssues();
+        oldIssues.add(newIssue.withId(issueId));
+        assertEquals(newIssues, oldIssues);
+    }
+
+
+}
---
+++ rest-sample/src/test/java/pft/rest/TestBase.java
@@ -0,0 +1,56 @@
+package pft.rest;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+import com.google.gson.reflect.TypeToken;
+import org.apache.http.client.fluent.Executor;
+import org.apache.http.client.fluent.Request;
+import org.apache.http.message.BasicNameValuePair;
+import org.testng.SkipException;
+
+import java.io.IOException;
+import java.math.BigInteger;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.rmi.RemoteException;
+import java.util.Set;
+
+/**
+ * Created by kshyniakov on 24.01.2017.
+ */
+public class TestBase {
+    protected Set<Issue> getIssues() throws IOException {
+        String json = getExecutor().execute(Request.Get("http://demo.bugify.com/api/issues.json")).returnContent().asString();
+        JsonElement parsed = new JsonParser().parse(json);
+        JsonElement issues = parsed.getAsJsonObject().get("issues");
+        return new Gson().fromJson(issues, new TypeToken<Set<Issue>>(){}.getType());
+    }
+
+    private Executor getExecutor(){
+        return Executor.newInstance().auth("LSGjeU4yP1X493ud1hNniA==","");
+    }
+
+    protected int createIssue(Issue newIssue) throws IOException {
+        String json = getExecutor().execute(Request.Post("http://demo.bugify.com/api/issues.json")
+                .bodyForm(new BasicNameValuePair("subject", newIssue.getSubject()), new BasicNameValuePair("description", newIssue.getDescription())))
+                .returnContent().asString();
+        JsonElement parsed = new JsonParser().parse(json);
+        return parsed.getAsJsonObject().get("issue_id").getAsInt();
+    }
+
+    public void skipIfNotFixed(int issueId) throws IOException {
+        if (isIssueOpen(issueId)) {
+            throw new SkipException("Ignored because of issue " + issueId);
+        }
+    }
+
+    private boolean isIssueOpen(int issueId) throws IOException {
+        String json = getExecutor().execute(Request.Get("http://demo.bugify.com/api/issues/"+issueId+".json")).returnContent().asString();
+        JsonElement parsed = new JsonParser().parse(json);
+        //JsonElement issue
+        String s= parsed.getAsJsonObject().get("issues").getAsString().g;
+        // = issue.getAsJsonObject().get("state_name").getAsString();
+        return !issue.getAsJsonObject().get("state_name").getAsString().equals("Resolved");
+    }
+}
